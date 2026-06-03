4160ec18d8f944cc28d72fde1ec1ea68bd05ebc9
Gefei Kong <gefei.kong@heigit.org>
2026-06-03 18:04:23 +0200

    feat(score): update POI categories

--- xmin_core/poi_categories/two_levels.py
+++ xmin_core/poi_categories/two_levels.py
@@ -10,8 +10,7 @@ CATEGORY_BENCHMARKS = {
 
 
 class TwoLvlFacilitiesCategories(POICatogories):
-    healthcare = Category()
-
+    # todo: the sum of sub_weights in each category is not always 1.0, need to check.
     education = Category(
         weight=0.2,
         subcategories=[
@@ -35,9 +34,7 @@ class TwoLvlFacilitiesCategories(POICatogories):
                 tag="amenity=childcare or amenity=nursery",
             ),
             SubCategory(
-                name="early_education",
-                sub_weight=0.5,
-                tag="amenity=kindergarten or building=kindergarten",
+                name="early_education", sub_weight=0.5, tag="amenity=kindergarten"
             ),
             SubCategory(
                 name="play",
@@ -47,13 +44,116 @@ class TwoLvlFacilitiesCategories(POICatogories):
         ],
     )
 
-    daily_living = Category()
+    healthcare = Category(
+        weight=0.1,
+        subcategories=[
+            SubCategory(
+                name="primary_care",
+                sub_weight=0.3,
+                tag="(amenity=doctors or healthcare=doctor or amenity=clinic ) and "
+                "(healthcare:speciality=general or healthcare:speciality=internal)",
+            ),
+            SubCategory(
+                name="dentist",
+                sub_weight=0.2,
+                tag="amenity=dentist or healthcare=dentist",
+            ),
+            SubCategory(
+                name="pharmacy",
+                sub_weight=0.5,
+                tag="amenity=pharmacy or healthcare=pharmacy",
+            ),
+            SubCategory(
+                name="specialist",
+                sub_weight=0.1,
+                tag="healthcare:speciality=* and (healthcare:speciality!=general or healthcare:speciality=internal)",
+            ),
+        ],
+    )
 
-    public_transport = Category()
+    daily_living = Category(
+        weight=0.4,
+        subcategories=[
+            # todo: grocery will be seperated to 3 level: large, medium, and specialty. The levels OR following update should consider it.
+            SubCategory(name="grocery_large", sub_weight=0.8, tag="shop=supermarket"),
+            SubCategory(
+                name="grocery_medium",
+                sub_weight=0.8,
+                tag="amenity=marketplace or shop=convenience",
+            ),
+            SubCategory(
+                name="grocery_specialbase",
+                sub_weight=0.8,
+                tag="shop in "
+                "(cheese, coffee, deli, farm, frozen_food, health_food, organic, "
+                "water, bakery, butcher, dairy, food, greengrocer, grocery, seafood)",
+            ),
+            SubCategory(
+                name="grocery_specialluxury",
+                sub_weight=0.8,
+                tag="shop in (alcohol, beverages, chocolate, ice_cream, pastry, spices, tea, water, wine, confectionery)",
+            ),
+            # all above are related to groceery
+            SubCategory(
+                name="post",
+                sub_weight=0.1,
+                tag="amenity=post_box or amenity=post_office",
+            ),
+            SubCategory(
+                name="drugstore", sub_weight=0.1, tag="shop=drugstore or shop=chemist"
+            ),
+            SubCategory(
+                name="money", sub_weight=0.05, tag="amenity=atm or amenity=bank"
+            ),
+            SubCategory(
+                name="various",
+                sub_weight=0.1,
+                tag="shop in (hairdresser, dry_cleaning,  copyshop) or "
+                "craft in (shoemaker, tailor, electronics_repair, photographer)",
+            ),
+        ],
+    )
 
-    green_space = Category()
+    public_transport = Category(
+        weight=0.1,
+        subcategories=[
+            SubCategory(name="bus_stop", sub_weight=0.1, tag="highway=bus_stop"),
+            SubCategory(name="tram_stop", sub_weight=0.25, tag="railway=tram_stop"),
+            SubCategory(
+                name="train_station",
+                sub_weight=0.7,
+                tag="station=subway or railway=station or railway=halt or railway=stop",
+            ),
+        ],
+    )
 
-    culture_leisure = Category()
+    nature_space = Category(
+        weight=0.05,
+        subcategories=[
+            SubCategory(
+                name="all",
+                sub_weight=1,
+                tag="",  # todo, update nature space query tag.
+            ),
+        ],
+    )
+
+    culture_leisure = Category(
+        weight=0.02,
+        subcategories=[
+            SubCategory(
+                name="culture_entertainment",
+                sub_weight=0.5,
+                tag="amenity in (cinema, library, public_bookcase, community_centre) or"
+                "museum=culture or tourism=museum or amenity in (planetarium, theatre, arts_centre)",
+            ),
+            SubCategory(
+                name="sport_recreation",
+                sub_weight=0.5,
+                tag="leisure in (fitness_centre, fitness_station, sports_centre, swimming_pool)",
+            ),
+        ],
+    )
 
     eating_out = Category()
 
