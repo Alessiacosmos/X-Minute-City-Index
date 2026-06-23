52e319382c01704cb6f61f1ba23279d06a0901a5
Gefei Kong <gefei.kong@heigit.org>
2026-06-23 10:00:23 +0200

    chore: adjust slurm running time & reduce timeframe number

--- slurm/run_cli_60_cities.sh
+++ slurm/run_cli_60_cities.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name xmin:cli_60_cities
 #SBATCH --partition=cpu-single
-#SBATCH --time=24:00:00
+#SBATCH --time=18:00:00
 #SBATCH --mem=8gb
 #SBATCH --mail-user gefei.kong@heigit.org
 #SBATCH --mail-type ALL         # ALL will alert you of job beginning, completion, failure etc
